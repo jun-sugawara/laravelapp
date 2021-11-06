6c2831b6c72229f6b7fa0ceaea7c2f0f82663567
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-11-06 13:46:59 +0900

    複数コントローラーの利用でアクセスする練習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+global $head, $style, $body, $end;
+$head ='<html><head>';
+$style = <<<EOF
+<style>
+    body {font-size:16px; color:#999 }
+    h1 {font-size:100px; text-align:right; color:#eee; 
+        margin:-40px 0px -50px 0px; } 
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+function tag($tag, $txt){
+    return "<{$tag}>". $txt . "</{$tag}>";
+}
 
 class HelloController extends Controller
 {
-    //
+    public function index(){
+        global $head, $style, $body, $end;
+        
+        $html = $head . tag('title','Hello/Index') . $style .
+            $body
+            . tag('h1','Index'). tag('p','this is index page')
+            . '<a href="/hello/other">go to other page</a>'
+            . $end;
+        return $html;
+    }
+
+    public function other(){
+        global $head, $style, $body, $end;
+        
+        $html = $head . tag('title','Hello/Other'). $style .
+            $body
+            . tag('h1','Other'). tag('p','this is Other page')
+            . $end;
+        return $html;
+    }
 }
+    
--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('hello/{msg?}',function($msg='メッセージがありません'){
-$html = <<< EOF
-<html>
-    <head>
-       <title>Hello</title>
-            <style>
-                body {font-size:16px; color:#999 }
-                h1 {font-size:100px; text-align:right; color:#eee; margin:-40px 0px -50px 0px } 
-            </style>
-    </head>
-    <body>
-        <h1>Hello</h1>
-        <p>$msg</p>
-        <p>これはサンプルページです</p>
-    </body>
-</html>
-EOF;
-
-    return $html;
-});
+Route::get('hello','App\Http\Controllers\HelloController@index');
+Route::get('hello/other','App\Http\Controllers\HelloController@other');
