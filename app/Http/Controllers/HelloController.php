263045bf30dfbb3bf5791038e2245c0d5ccd4b75
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-11-07 21:17:51 +0900

    bladeを使って表示

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,26 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
 
-public function __invoke() {
-    
-        return <<<EOF
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body {font-size:16px; color:#999;}
-    h1 {font-size:30px; text-align:right; color:#eee; margin:-15px 0px 0px 0px;}
-    </style>
-    </head>
-    <body>
-        <h1>Single Action</h1>
-        <p>これはシングルアクションコントローラーのアクションです</p>
-    </body>
-    </html>
-    EOF;
+    public function index() {
+        return view('hello.index');
     }
 }
