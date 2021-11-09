89ad93acd985c37cd461bf057c98f1b6c21f5a84
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-11-10 07:34:09 +0900

    bladeの構文の中でif文を使用する練習

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <title>Hello/Index</title>
+  <style>
+  body { font-size:16px; color:#999 }
+  h1 { font-size:50px; text-align:right; color:#f6f6f6; margin:-20px 0px -30px 0px; }
+  </style>
+</head>
+  <body>
+    <h1>Blade/Index</h1>
+    @if($msg != '')
+      <p>こんにちは{{$msg}}さん</p>
+    @else
+      <p>何か書いてください</p>
+    @endif
+      <form method="POST" action="/hello">
+      @csrf
+        <input type="text" name="msg">
+        <input type="submit">
+      </form>  
+  </body>
+</html>
