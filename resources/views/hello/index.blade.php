02d6510a863cbd48963a0319a8350e679295ac30
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-11-12 07:40:43 +0900

    @phpディレクティブを使ってhtmlに直接phpのコードを入れてみる

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,18 +8,20 @@
 </head>
   <body>
     <h1>Blade/Index</h1>
-    <p>&#064;forディレクティブの部分</p>
-    @foreach($data as $item)
-    @if($loop->first)
-    <p>※データ一覧</p>
-    <ul>
-    @endif
-    <li>No,{{$loop->iteration}} . {{$item}}</li>
-    @if($loop->last)
-    </ul
-    ><p>ーーーーーーーここまで</p>
-    @endif
-    @endforeach
+    <p>&#064;whileディレクティブの例</p>
+    <ol>
+      @php 
+      $counter = 0;
+      @endphp
+      @while($counter < count($data))
+      <li>{{$data[$counter]}}</li>
+      @php
+      $counter++;
+      @endphp
+      @endwhile
+    </ol>
+
+
       <form method="POST" action="/hello">
       @csrf
         <input type="text" name="msg">
