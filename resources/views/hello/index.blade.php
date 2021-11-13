3725597c4072ab347213dfd8df16a77ad5ff4722
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-11-13 14:37:36 +0900

    componentを用いたテンプレートの練習

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,26 +1,3 @@
-<!-- <html>
-<head>
-  <title>Hello/Index</title>
-  <style>
-  body { font-size:16px; color:#999 }
-  h1 { font-size:50px; text-align:right; color:#f6f6f6; margin:-20px 0px -30px 0px; }
-  </style>
-</head>
-  <body>
-    <h1>Blade/Index</h1>
-    <p>&#064;whileディレクティブの例</p>
-    <ol>
-      @php 
-      $counter = 0;
-      @endphp
-      @while($counter < count($data))
-      <li>{{$data[$counter]}}</li>
-      @php
-      $counter++;
-      @endphp
-      @endwhile
-    </ol>
-</html> -->
 @extends('layouts.helloapp')
 @section('title', 'Index')
 @section('menubar')
@@ -31,6 +8,17 @@
 @section('content')
   <p>ここが本文のコンテンツです</p>
   <p>必要なだけ記述できます</p>
+
+  @component("components.message")
+    @slot('msg_title')
+    CAUTION!
+    @endslot
+
+    @slot("msg_content")
+    これはメッセージの表示です
+    @endslot
+  @endcomponent
+  
 @endsection
 
 @section('footer')
