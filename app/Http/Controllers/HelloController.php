ede6255c31ad770feb8e227a1e43efd89afc38a7
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-11-13 15:14:47 +0900

    eachを使った表示の練習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,12 @@ class HelloController extends Controller
 {
 
     public function index() {
-        $data = ['one','two','three','four','five'];
+        $data = [
+            ['name'=>"すがわらじゅん", "mail"=>"24karats@gmail.com"],
+            ['name'=>"ソナムワンギャル", "mail"=>"sonum@gmail.com"],
+            ['name'=>"なっちゃん", "mail"=>"natsumi@gmail.com"],
+            ['name'=>"とおる", "mail"=>"toru@gmail.com"]
+        ];
         return view('hello.index',['data'=>$data]);
     }
 
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,9 +7,9 @@
 
 @section('content')
   <p>ここが本文のコンテンツです</p>
-  <p>必要なだけ記述できます</p>
-
-  @include("components.message", ["msg_title"=> "OK", "msg_content"=> "さぶびゅーです"])
+  <ul>
+    @each('components.item', $data, 'item')
+  </ul>
 
 @endsection
 
