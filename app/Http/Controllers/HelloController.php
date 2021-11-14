bed8d265ed1d6bc26ae2003b4afa5bbe2ef10e55
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-11-14 13:58:30 +0900

    HelloServiceProviderを用いて画面の表示

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,13 +9,7 @@ class HelloController extends Controller
 {
 
     public function index() {
-        $data = [
-            ['name'=>"すがわらじゅん", "mail"=>"24karats@gmail.com"],
-            ['name'=>"ソナムワンギャル", "mail"=>"sonum@gmail.com"],
-            ['name'=>"なっちゃん", "mail"=>"natsumi@gmail.com"],
-            ['name'=>"とおる", "mail"=>"toru@gmail.com"]
-        ];
-        return view('hello.index',['data'=>$data]);
+        return view('hello.index',['message'=>'Hello']);
     }
 
     public function post(Request $request){
